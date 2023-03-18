8f9b7868ec8b3e3b18b15e80a26bd3d240249768
Nidhithakur71 <126388476+Nidhithakur71@users.noreply.github.com>
2023-03-18 09:05:25 +0530

    Add files via upload

---
+++ assigdive.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+	int r,s,t;
+	
+	printf("enter two number to divide\n");
+	scanf("%d%d",&r,&s);
+	
+	t = r / s; 
+	
+	printf("sub of the number = %d\n" , t);
+	return 0; 
+}
+
