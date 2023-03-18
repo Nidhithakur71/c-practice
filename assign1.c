b032881bb16e12ee35b59f6c51e9ca43b3947437
Nidhithakur71 <126388476+Nidhithakur71@users.noreply.github.com>
2023-03-18 09:13:27 +0530

    Add files via upload

---
+++ assign1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a ,n;
+	printf("input the value for a and n: ");
+	scanf("%d %d",&a,&n);
+	if(a==n)
+	printf("a and n are equal\n");
+	else
+	printf("a and n are not equal\n");
+	return 0;
+
+}
