82f1c544904fe74496ce7342ae8f3d179d822770
Nidhithakur71 <126388476+Nidhithakur71@users.noreply.github.com>
2023-03-18 11:15:15 +0530

    Add files via upload

---
+++ flot.c
@@ -0,0 +1,15 @@
+# include<stdio.h>
+
+int main()
+{
+	float n1, n2, n;
+	printf("enter the n1:");
+	scanf("%f",&n1);
+	printf("enter the n2:");
+	scanf("%f",&n2);
+	
+	n=n1*n2;
+	printf("the n of n1 ans n2 is:%.2f",n);
+	return 0;
+	
+}
